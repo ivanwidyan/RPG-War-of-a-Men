1bd32663c0282729452b19051279167525aa5134
Ivan Widyan <ivanwidyan@yahoo.com>
2017-08-09 00:03:23 +0700

    Using Cursor Affordances

---
+++ RPG War of a Men/Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+
+    CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+
+    // TODO fix issue with click to move and WASD conflicting and increasing speed
+	
+	// Update is called once per frame
+	void Update () {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.Log("Cursor error");
+                return;
+        }
+        
+	}
+}
